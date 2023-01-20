b49e9da4dc522b4761735b91b2a71322c50a4c01
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-01-20 01:57:21 -0800

    sorting algorithm and big o

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sort a doubly linked list
+ * @list: Double linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ * swap_node - swap a node for the prev
+ * @node: node
+ * @list: node list
+ * Return: return a ptr to a node
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	back->next = current->next;
+
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
